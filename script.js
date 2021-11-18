60e22bec9bf2a9f481fad365386a9c9129aaacff
Noway <nikav1999@yandex.ru>
2021-11-18 19:43:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const menu = document.getElementById('menu-btn')
+const navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
